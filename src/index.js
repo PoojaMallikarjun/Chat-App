ba77540a56bf5268e40fa689ff55efaac6eadb79
Pooja Mallikarjun <poojadotm702@gmail.com>
2020-07-08 19:45:46 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Login from "./login/login";
+import Signup from "./signup/signup";
+import Dashboard from "./dashboard/dashboard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD_ST6ieO1jUJkD8ifqGXybdEZLkgYHwnA",
+  authDomain: "react-chat-app-ae5eb.firebaseapp.com",
+  databaseURL: "https://react-chat-app-ae5eb.firebaseio.com",
+  projectId: "react-chat-app-ae5eb",
+  storageBucket: "react-chat-app-ae5eb.appspot.com",
+  messagingSenderId: "564341132269",
+  appId: "1:564341132269:web:7a38dfc6a4808c69dcf5e4",
+  measurementId: "G-XEGZWV6R1R",
+});
+
+const routing = (
+  <Router>
+    <div id="routing-container">
+      <Route path="/login" component={Login}></Route>
+      <Route path="/signup" component={Signup}></Route>
+      <Route path="/dashboard" component={Dashboard}></Route>
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/signup/signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseLine from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import styles from "./styles";
+const firebase = require("firebase");
+
+export default class Signup extends Component {
+  render() {
+    return <div></div>;
+  }
+}
